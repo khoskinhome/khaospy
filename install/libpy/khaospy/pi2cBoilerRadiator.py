388f81c7bf0e36e567203125d95439f3c2e93f10
Karl Hoskin <karl@khoskin.co.uk>
2015-06-14 00:01:08 +0100

    minor updates

---
+++ install/libpy/khaospy/pi2cBoilerRadiator.py
@@ -0,0 +1,19 @@
+"""
+khaospy.pi2cBoilerRadiator
+is going to be very similar to
+khaospy.pi2cMultiLightSingleWayManual
+
+They are both solving a similar problem.
+
+Although for boiler/radiator there is just one circuit.
+
+
+
+
+
+For a boiler this class will get instantiated twice, once for "hot water" the other for "heating".
+
+
+
+
+"""
