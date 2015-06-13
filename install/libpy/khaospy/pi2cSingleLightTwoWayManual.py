dd70515fc47e761f77d97337ecc1f2a03ba653a3
Karl Hoskin <karl@khoskin.co.uk>
2015-06-13 23:50:53 +0100

    lower casing Pi2c to pi2c

--- install/libpy/khaospy/pi2cSingleLightTwoWayManual.py
+++ install/libpy/khaospy/pi2cSingleLightTwoWayManual.py
@@ -12,45 +12,45 @@ class Device(object):
 
     def getSwitchMainsState(self) :
         # throws an error.
-        raise khaospyUtils.Kaboom("Pi2cSingleLightTwoWayManual cannot getSwitchMainsState")
+        raise khaospyUtils.Kaboom("pi2cSingleLightTwoWayManual cannot getSwitchMainsState")
 
     def getSwitchExtraState(self) :
         # returns the state of the extra low voltage switch. ( on(true) or off(false) )
-        print ("Pi2cSingleLightTwoWayManual.getSwitchExtraState")
+        print ("pi2cSingleLightTwoWayManual.getSwitchExtraState")
         return False
 
     def pollMainsDetector(self):
-        print ("Pi2cSingleLightTwoWayManual.pollMainsDetector")
+        print ("pi2cSingleLightTwoWayManual.pollMainsDetector")
         return True;
 
 #    def pollSwitchAndToggle(self) :
 #        # polls the state of Li
-#        print ("Pi2cSingleLightTwoWayManual.pollSwitchAndToggle")
+#        print ("pi2cSingleLightTwoWayManual.pollSwitchAndToggle")
 #        return False
 
 #    def areLightsMainlyOn(self) :
 #        # should this be called "isLightsMainlyOn" ? that sounds wrong to me.
 #        # returns True or False. If 5 lights out of a possible 9 are "ON" this returns True. If 4 lights out of a possible 9 are "ON" this method returns False.
-#        print ("Pi2cSingleLightTwoWayManual.areLightsMainlyOn")
+#        print ("pi2cSingleLightTwoWayManual.areLightsMainlyOn")
 #        return False
 
     # if the list-of-lights isn't defined in the following method calls, the default is ALL lights.
 
     def getLightState ( self ) :
         # returns an the state of the Light ( on(true) or off(false) ) . This is what the 240v-detector detects
-        print ("Pi2cSingleLightTwoWayManual.getLightState" )
+        print ("pi2cSingleLightTwoWayManual.getLightState" )
         return False
 
     def setLightOn ( self ) :
-        print ("Pi2cSingleLightTwoWayManual.setLightOn")
+        print ("pi2cSingleLightTwoWayManual.setLightOn")
         return False
 
     def setLightOff ( self ) :
-        print ("Pi2cSingleLightTwoWayManual.setLightOff")
+        print ("pi2cSingleLightTwoWayManual.setLightOff")
         return False
 
     def setLightToggle ( self ) :
-        print ("Pi2cSingleLightTwoWayManual.setLightToggle")
+        print ("pi2cSingleLightTwoWayManual.setLightToggle")
         return False
         #  ( swaps light from current state. if light is on, this method switches it off )
 
