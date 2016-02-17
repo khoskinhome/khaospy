79b0a92873d66a90a7fb718231a6960965418927
Karl Hoskin <karl@khoskin.co.uk>
2016-02-17 21:26:17 +0000

    more on the ControlPiMCP23017
    still not complete.

--- install/lib-perl-t/KhaospyControlPiMCP23017.t
+++ install/lib-perl-t/KhaospyControlPiMCP23017.t
@@ -6,7 +6,7 @@ FindBin::again();
 use lib "$FindBin::Bin/../lib-perl/";
 # by Karl Kount-Khaos Hoskin. 2015-2016
 
-use Test::More tests => 9;
+use Test::More qw/no_plan/;
 use Test::Exception;
 use Test::Deep;
 
@@ -30,31 +30,50 @@ my $gpio = {
     portnum  => 5,
 };
 
+# Test init
 init_gpio(undef,$gpio,OUT);
 
 my $expect = {
     '0' => {
-         '0x20' => {
-             'b' => [
-                    1,
-                    1,
-                    1,
-                    1,
-                    1,
-                    0,
-                    1,
-                    1
-                  ]
-             }
-         }
-    };
-
+        '0x20' => {
+            'b' => [qw/1 1 1 1 1 0 1 1/ ]
+        }
+    }
+};
 
 cmp_deeply(
-    Khaospy::ControlPiMCP23017->get_pins_cfg(),
+    Khaospy::ControlPiMCP23017::testing_get_pins_cfg(),
     $expect,
-    "pins cfg looks okay" );
+    "pins cfg looks okay"
+);
 
+# Test writing to a gpio
+$gpio->{portnum} = 0;
+Khaospy::ControlPiMCP23017->write_gpio($gpio, true);
+
+my $pins_out = Khaospy::ControlPiMCP23017::testing_get_pins_out_state();
+cmp_deeply(
+    Khaospy::ControlPiMCP23017::get_pins_array($gpio, $pins_out),
+    [qw/1 0 0 0 0 0 0 0/],
+    "wrote to pins_out successfully"
+);
+
+# Test reading a gpio.
+# they should all get init-ed to 1 (true)
+# so set one to 0 (false)
+my $test_bit = 5;
+$gpio->{portnum} = $test_bit;
+my $pins_in = Khaospy::ControlPiMCP23017->testing_get_pins_in_state();
+Khaospy::ControlPiMCP23017::set_pins_state_array($gpio,$pins_in,false);
+
+for my $bit ( 0..7 ) {
+    $gpio->{portnum} = $bit;
+    my $expect = $bit == $test_bit ? false : true;
+    my $result = Khaospy::ControlPiMCP23017->read_gpio($gpio, $pins_in);
+    ok( $result == $expect, "read bit $bit == $expect" );
+}
+
+# test the number-to-array and array-to-number conversions :
 cmp_deeply(
     Khaospy::ControlPiMCP23017::_num_to_pin_array(255),
     [qw/1 1 1 1 1 1 1 1/],
--- install/lib-perl/Khaospy/ControlPiMCP23017.pm
+++ install/lib-perl/Khaospy/ControlPiMCP23017.pm
@@ -9,8 +9,6 @@ use Data::Dumper;
 use Carp qw/confess/;
 use Time::HiRes qw/time/;
 
-use Clone qw/clone/;
-
 use Khaospy::Constants qw(
     $PI_I2C_GET
     $PI_I2C_SET
@@ -35,13 +33,12 @@ our @EXPORT_OK = qw(
     write_gpio
 );
 
-
 #=pod
 #        my $switch_state = qx{i2cget -y $i2c_bus_y 0x20 0x12};
 #=cut
 
 #=pod
-#$mcp23017_pins_[config|state] = {
+# $mcp23017_pins_[config|state] = {
 #    <i2c_bus> => {
 #        <i2c_addr> => {
 #            <port_a|port_b> => [
@@ -54,17 +51,24 @@ our @EXPORT_OK = qw(
 #        }
 #    },
 #
-#}
+# }
 #
-#    mcp_OLAT | mcp_GPIO | mcp_IODIR = {
-#        <i2c_bus> => {
-#            <i2c_addr> => {
-#                    val =>  integer,
-#                    time => epoch_secs # since last updated. INs read, OUTs written.
-#                }
-#            }
-#        }
+# mcp_OLAT | mcp_GPIO | mcp_IODIR = {
+#     <i2c_bus> => {
+#         <i2c_addr> => {
+#                 val =>  integer,
+#                 time => epoch_secs # since last updated. INs read, OUTs written.
+#             }
+#         }
+#     }
 #
+# gpio = { # something like :
+#    i2c_bus  => 0,
+#	 i2c_addr => '0x20',
+#	 portname =>'b',
+#	 portnum  => 1,
+# };
+
 
 #=cut
 
@@ -80,58 +84,49 @@ my $GPIO  = { a => '0x12', b => '0x13' };
 # OLAT A/B are used to switch on and off the outputs on a gpio port.
 my $OLAT  = { a => '0x14', b => '0x15' };
 
-my $mcp_IODIR ={};
-my $mcp_GPIO = {};
-my $mcp_OLAT = {};
+my $mcp_IODIR = {};
+my $mcp_GPIO  = {};
+my $mcp_OLAT  = {};
 
-my $pins_cfg   = {};
+my $pins_cfg       = {}; # used by IODIR[A|B]
+my $pins_out_state = {}; # used by OLAT[A|B]
+my $pins_in_state  = {}; # used by GPIO[A|B]
 
-my $pins_state = {};
-
-sub get_pins_cfg {
-    # needed for testing.
-    return clone $pins_cfg;
-}
-
-sub get_pins_state {
-    # needed for testing.
-    return clone $pins_state;
-}
+# needed for testing.
+sub testing_get_pins_cfg       { $pins_cfg }
+sub testing_set_pins_cfg       { $pins_cfg = $_[0] }
+sub testing_get_pins_in_state  { $pins_in_state }
+sub testing_set_pins_in_state  { $pins_in_state = $_[0] }
+sub testing_get_pins_out_state { $pins_out_state }
+sub testing_set_pins_out_state { $pins_out_state = $_[0] }
 
 sub init_gpio {
-    my ($class, $gpio, $IN_OUT) = @_;
+    my ($class, $gpio, $IN_OUT ) = @_;
     # inits a single gpio pin
-    _init_pins( $gpio, $pins_cfg, $IN_OUT);
-    _init_pins( $gpio, $pins_state);
+    _init_pins( $gpio, $pins_cfg      , $IN_OUT, true );
+    _init_pins( $gpio, $pins_in_state ,undef   , true );
+    _init_pins( $gpio, $pins_out_state,undef   , false );
 }
 
 sub _init_pins {
-    my ($gpio, $pins_i, $IN_OUT) = @_;
+    my ( $gpio, $pins_i, $IN_OUT, $default_bit ) = @_;
 
-    # gpio = { # something like :
-    #    i2c_bus  => 0,
-    #	 i2c_addr => '0x20',
-    #	 portname =>'b',
-    #	 portnum  => 1,
-    # };
+    confess "programming error. default_bit ($default_bit) not set correctly "
+        if ! defined $default_bit || $default_bit !~ /^[01]$/g;
 
     $pins_i->{$gpio->{i2c_bus}} = {}
         if ! exists $pins_i->{get_hashval($gpio, 'i2c_bus')};
-
     my $i2c_bus_rh = $pins_i->{$gpio->{i2c_bus}};
 
-    ##
     $i2c_bus_rh->{$gpio->{i2c_addr}} = {}
         if ! exists $i2c_bus_rh->{get_hashval($gpio, 'i2c_addr')};
-
     my $i2c_addr_rh = $i2c_bus_rh->{$gpio->{i2c_addr}};
 
+    $gpio->{portname} = lc(get_hashval($gpio, 'portname'));
     # Default of 11111111 0xff , so the IODIR defaults to input.
     # this is electronically safer for the MCP23017
-    $i2c_addr_rh->{lc($gpio->{portname})} = [ 1,1,1,1,1,1,1,1 ]
-        if ! exists $i2c_addr_rh->{get_hashval($gpio, 'portname')};
-
-    $gpio->{portname} = lc($gpio->{portname});
+    $i2c_addr_rh->{lc($gpio->{portname})} = [ map { $default_bit } 0..7 ]
+        if ! exists $i2c_addr_rh->{$gpio->{portname}};
 
     return if ! defined $IN_OUT;
 
@@ -140,55 +135,79 @@ sub _init_pins {
 
     $i2c_addr_rh->{$gpio->{portname}}[ get_hashval($gpio,'portnum')]
         = ( $IN_OUT eq IN ) ? true : false ;
-
 }
 
 sub init_mcp23017 {
 
     print Dumper ( $pins_cfg );
-
     # TODO actually push the pin config to the chip
 
 }
 
 sub poll_mcp23017 {
-    # reads the INs , writes the OUTs
+    # TODO reads the INs , writes the OUTs
 
 }
 
+sub get_pins_i_name {
+    my ( $pins_i ) = @_;
+    return 'pins_cfg'       if $pins_i == $pins_cfg;
+    return 'pins_in_state'  if $pins_i == $pins_in_state;
+    return 'pins_out_state' if $pins_i == $pins_out_state;
+    return 'unknown-pins-datastructure';
+}
 
+sub get_pins_array {
+    my ($gpio, $pins_i) = @_;
 
+    klogfatal get_pins_i_name($pins_i)." doesn't have i2c_bus set"
+        if ! exists $pins_i->{get_hashval($gpio, 'i2c_bus')};
+    my $i2c_bus_rh = $pins_i->{$gpio->{i2c_bus}};
 
-#    $IN_OUT = lc( $IN_OUT );
-#    klogfatal "Can only set a Pi GPIO mode ($IN_OUT) to 'in' or 'out'"
-#        if $IN_OUT ne IN and $IN_OUT ne OUT;
-#
-#    system("$PI_GPIO_CMD mode $gpio_num $IN_OUT");
-#}
+    klogfatal get_pins_i_name($pins_i)." doesn't have i2c_addr set"
+        if ! exists $i2c_bus_rh->{get_hashval($gpio, 'i2c_addr')};
+    my $i2c_addr_rh = $i2c_bus_rh->{$gpio->{i2c_addr}};
+
+    $gpio->{portname} = lc(get_hashval($gpio, 'portname'));
+    klogfatal get_pins_i_name($pins_i)." doesn't have portname set"
+        if ! exists $i2c_addr_rh->{$gpio->{portname}};
+    my $array_ra = $i2c_addr_rh->{$gpio->{portname}};
+
+    return $array_ra;
+}
+
+sub set_pins_state_array {
+    my ( $gpio, $pins_i, $new_state ) = @_;
+
+    klogfatal "new_state ($new_state) isn't 1 or 0"
+        if $new_state != true and $new_state != false;
+
+    # Should never need to set the pins_cfg after init.
+    klogfatal get_pins_i_name($pins_i)." is not settable"
+        if $pins_i != $pins_in_state
+            and $pins_i != $pins_out_state;
+
+    get_pins_array($gpio, $pins_i)->[ get_hashval($gpio,'portnum') ]
+        = $new_state;
+}
 
 sub read_gpio {
-    my ($class, $gpio_num) = @_;
-    # reads a single gpio pin
-#    my $r = qx( $PI_GPIO_CMD read $gpio_num );
-#    chomp $r;
-#    return $r;
+    my ( $class, $gpio ) = @_;
+
+    return get_pins_array( $gpio, $pins_in_state )
+        ->[ get_hashval($gpio, 'portnum') ];
 }
 
 sub write_gpio {
-    my ($class, $gpio_num, $val) = @_;
-    # writes a single gpio pin
-#    system("$PI_GPIO_CMD write $gpio_num $val");
-#    return;
+    my ( $class, $gpio, $new_state ) = @_;
+    set_pins_state_array($gpio, $pins_out_state, $new_state);
 }
 
-
-#_pin_array_to_num  and  _num_to_pin_array {
-# convert an array with 8 elements  [ 0..7]
-# to a number and vice-versa
-# where element 0 == bit 0
 sub _pin_array_to_num {
     my ( $array ) = @_;
-    confess "array doesn't have 8 elements" if @$array != 8;
+
+    klogfatal "Array doesn't have 8 bits for gpio, or a bit is not either 1 or 0",
+        $array if @$array != 8 || grep { $_ != true and $_ != false } @$array;
 
     return oct("0b".join( "", reverse @$array));
 }
@@ -196,13 +215,12 @@ sub _pin_array_to_num {
 sub _num_to_pin_array {
     my ( $num ) = @_;
 
-    confess "number is not between 0 and 255"
+    klogfatal "number is not between 0 and 255"
         if $num < 0 || $num > 255;
 
     return [ reverse split ( //, sprintf("%08b", $num ) )];
 
 }
-
 
 1;
 
